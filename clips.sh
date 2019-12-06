75a5faf5606f4b0acaa9a18242f998002b1dcfd6
iMorph <iMorpheus@users.noreply.github.com>
2019-12-06 12:55:33 +0900

    Update clips.sh
    Comments

--- clips.sh
+++ clips.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Purpose: Download Twitch clip to local machine
+# Dependency: youtube-dl
+
 function Clipper()
 {
 read -p "Clip Name: " -e ClipName # Name of file
 read -p "Clip URL: " -e ClipURL # Clip URL
 (cd [path to download directory] ; youtube-dl -o "$ClipName.mp4" $ClipURL) # Change directory then download and name clip
-cd /Volumes/GT_Sport/Clips/ # Move directly to Clips Directory
+cd [path to download directory] # Move directly to Clips Directory
 
 # Catalog/Curate to clips.json
 echo {\"Title\": \"$ClipName\", \"URL\": \"$ClipURL\"} >> clips.json # Appending because I don't know how to use jq
