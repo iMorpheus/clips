41158b67e536ae18c0a61129f8e57e19e33c0712
iMorph <iMorpheus@users.noreply.github.com>
2019-12-06 12:52:59 +0900

    Update clips.sh
    Added save to json file to catalog library of clips

--- clips.sh
+++ clips.sh
@@ -5,6 +5,14 @@ function Clipper()
 read -p "Clip Name: " -e ClipName # Name of file
 read -p "Clip URL: " -e ClipURL # Clip URL
 (cd [path to download directory] ; youtube-dl -o "$ClipName.mp4" $ClipURL) # Change directory then download and name clip
+cd /Volumes/GT_Sport/Clips/ # Move directly to Clips Directory
+
+# Catalog/Curate to clips.json
+echo {\"Title\": \"$ClipName\", \"URL\": \"$ClipURL\"} >> clips.json # Appending because I don't know how to use jq
+
+# TODO
+# Add object to clips.json via jq
+# Nest objects in array thingy
 }
 
 Clipper
